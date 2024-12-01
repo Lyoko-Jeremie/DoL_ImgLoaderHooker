c88a7e1da96c14c98fecc6b9302089044a45a3a7
LyokoJeremie <lucheng989898@gmail.com>
2024-12-02 00:09:01 +0800

    example

--- doc/add-dynamic-replace-passage-name.js
+++ doc/add-dynamic-replace-passage-name.js
@@ -14,3 +14,6 @@ window.modImgLoaderHooker.addDynamicImageTagReplacePassage("onlyOnePassageName")
 
 // call `replaceAllImageInHtmlElement` to replace all image tag in the special html element
 window.modImgLoaderHooker.replaceAllImageInHtmlElement(document.body).then(r => {}).catch(e => console.error(e));
+// or
+await window.modImgLoaderHooker.replaceAllImageInHtmlElement(document.body);
+
