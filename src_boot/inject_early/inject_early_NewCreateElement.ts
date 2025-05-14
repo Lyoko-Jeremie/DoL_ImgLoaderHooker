1b8ca63178e7a3ce17d78842561305f5ae0ae7fe
LyokoJeremie <lucheng989898@gmail.com>
2025-05-14 23:14:39 +0800

    NewCreateElement

--- src_boot/inject_early/inject_early_NewCreateElement.ts
+++ src_boot/inject_early/inject_early_NewCreateElement.ts
@@ -17,91 +17,22 @@
         }
 
         function createImageElementProxy(imgElement: HTMLImageElement) {
-            const r = new Proxy<HTMLImageElement>(imgElement, {
-                set(target: HTMLImageElement, p: string | symbol, value: any, receiver: any): boolean {
-                    if (p !== 'src') {
-                        return Reflect.set(target, p, value);
-                        // (target as any)[p] = value;
-                        // return true;
-                        // return Reflect.set(target, p, value);
-                    }
-
-                    Reflect.apply(Reflect.get(target, 'setAttribute'), target, ['ml-src', value]);
-                    Reflect.apply(Reflect.get(target, 'setAttribute'), target, ['ml-lazy-src', value]);
+            const r = Object.defineProperty(imgElement, 'src', {
+                get(): any {
+                    return Reflect.get(this, 'src');
+                },
+                set(value: any) {
+                    // console.log('[Image] set src', [value]);
+                    Reflect.apply(Reflect.get(this, 'setAttribute'), this, ['ml-src', value]);
+                    Reflect.apply(Reflect.get(this, 'setAttribute'), this, ['ml-lazy-src', value]);
+                    Reflect.apply(Reflect.get(this, 'setAttribute'), this, ['src', value]);
                     window.modUtils.pSC2DataManager.getHtmlTagSrcHook().requestImageBySrc(value).then(imgString => {
-                        Reflect.apply(Reflect.get(target, 'setAttribute'), target, ['src', value]);
+                        // console.log('[Image] set src [src]', value, imgString);
+                        Reflect.apply(Reflect.get(this, 'setAttribute'), this, ['src', imgString ?? value]);
                     });
-
-                    // const imgObj = new Image();
-                    // will proxy by inject_early_NewImage.ts
-                    // imgObj.src = newValue;
-                    // receiver.setAttribute('ml-src', newValue);
-                    // receiver.setAttribute('ml-lazy-src', newValue);
-                    // imgObj.onload = () => {
-                    //     receiver.width = imgObj.width;
-                    //     receiver.height = imgObj.height;
-                    //     // receiver.setAttribute('src', newValue);
-                    //     Reflect.apply(Reflect.get(receiver, 'setAttribute'), receiver, ['src', imgObj.]);
-                    // };
-                    return true;
                 },
-                get(target: HTMLImageElement, p: string | symbol, receiver: any): any {
-                    console.log('get', [target, p, receiver]);
-                    const rr = Reflect.get(target, p, target);
-                    if (typeof rr === 'function') {
-                        rr.bind(target);
-                    }
-                    console.log('get rr', typeof rr);
-                    console.log('get target', typeof target);
-                    console.log('get imgElement', typeof imgElement);
-                    console.log('get rr', rr);
-                    return rr;
-                },
-                has(target: HTMLImageElement, p: string | symbol): boolean {
-                    console.log('has', [target, p]);
-                    return Reflect.has(target, p);
-                },
-                deleteProperty(target: HTMLImageElement, p: string | symbol): boolean {
-                    console.log('deleteProperty', [target, p]);
-                    return Reflect.deleteProperty(target, p);
-                },
-                ownKeys(target: HTMLImageElement): Array<string | symbol> {
-                    console.log('ownKeys', [target]);
-                    return Reflect.ownKeys(target);
-                },
-                getOwnPropertyDescriptor(target: HTMLImageElement, p: string | symbol): PropertyDescriptor | undefined {
-                    console.log('getOwnPropertyDescriptor', [target, p]);
-                    return Reflect.getOwnPropertyDescriptor(target, p);
-                },
-                setPrototypeOf(target: HTMLImageElement, v: any): boolean {
-                    console.log('setPrototypeOf', [target, v]);
-                    return Reflect.setPrototypeOf(target, v);
-                },
-                getPrototypeOf(target: HTMLImageElement): any {
-                    console.log('getPrototypeOf', [target]);
-                    return Reflect.getPrototypeOf(target);
-                },
-                isExtensible(target: HTMLImageElement): boolean {
-                    console.log('isExtensible', [target]);
-                    return Reflect.isExtensible(target);
-                },
-                preventExtensions(target: HTMLImageElement): boolean {
-                    console.log('preventExtensions', [target]);
-                    return Reflect.preventExtensions(target);
-                },
-                defineProperty(target: HTMLImageElement, p: string | symbol, attributes: PropertyDescriptor): boolean {
-                    console.log('defineProperty', [target, p, attributes]);
-                    return Reflect.defineProperty(target, p, attributes);
-                },
-                // apply(target: HTMLImageElement, thisArg: any, argArray?: any): any {
-                //     return Reflect.apply(target, thisArg, argArray);
-                // },
-                // construct(target: HTMLImageElement, argArray: any, newTarget: any): any {
-                //     return Reflect.construct(target, argArray, newTarget);
-                // },
             });
-            return Object.setPrototypeOf(r, Element.prototype);
-            // return r;
+            return r;
         }
 
         function NewCreateElement<K extends keyof HTMLElementTagNameMap>(tagName: K, options?: ElementCreationOptions): HTMLElementTagNameMap[K] {
