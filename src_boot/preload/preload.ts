d03117eb06ec685cbc3f466c5a7a208da1ee1439
LyokoJeremie <lucheng989898@gmail.com>
2025-09-29 22:30:43 +0800

    no renderer.js

--- src_boot/preload/preload.ts
+++ src_boot/preload/preload.ts
@@ -3,19 +3,19 @@
 
     window.modImgLoaderHooker.init();
 
-    const logger = window.modUtils.getLogger();
-    const scOld = window.modSC2DataManager.getSC2DataInfoAfterPatch();
+    // const logger = window.modUtils.getLogger();
+    // const scOld = window.modSC2DataManager.getSC2DataInfoAfterPatch();
 
-    const scNew = scOld.cloneSC2DataInfo();
-    {
-        const sc = scNew.scriptFileItems.getByNameWithNoPath('renderer.js');
-        if (!sc) {
-            console.error('[ImageLoaderHook] cannot find renderer.js');
-            logger.error('[ImageLoaderHook] cannot find renderer.js');
-            return;
-        }
-        sc.content = sc.content + '\n' + 'window.modImgLoaderHooker.installHook();' + '\n';
-    }
+    // const scNew = scOld.cloneSC2DataInfo();
+    // {
+    //     const sc = scNew.scriptFileItems.getByNameWithNoPath('renderer.js');
+    //     if (!sc) {
+    //         console.error('[ImageLoaderHook] cannot find renderer.js');
+    //         logger.error('[ImageLoaderHook] cannot find renderer.js');
+    //         return;
+    //     }
+    //     sc.content = sc.content + '\n' + 'window.modImgLoaderHooker.installHook();' + '\n';
+    // }
 
     // game/04-Variables/canvasmodel-patterns-lib.js
     // {
@@ -29,22 +29,22 @@
     // }
 
     // game/03-JavaScript/base.js
-    {
-        const sc = scNew.scriptFileItems.getByNameWithNoPath('base.js');
-        if (!sc) {
-            console.error('[ImageLoaderHook] cannot find base.js');
-            logger.error('[ImageLoaderHook] cannot find base.js');
-            return;
-        }
-        sc.content = sc.content.replace(
-            `newElem.setAttributeNS("http://www.w3.org/1999/xlink", "href", oldElem.attr("href") || oldElem.attr("xlink:href") || "");`,
-            'window.registerImagePatternSvgImage(oldElem, newElem);',
-        );
-    }
+    // {
+    //     const sc = scNew.scriptFileItems.getByNameWithNoPath('base.js');
+    //     if (!sc) {
+    //         console.error('[ImageLoaderHook] cannot find base.js');
+    //         logger.error('[ImageLoaderHook] cannot find base.js');
+    //         return;
+    //     }
+    //     sc.content = sc.content.replace(
+    //         `newElem.setAttributeNS("http://www.w3.org/1999/xlink", "href", oldElem.attr("href") || oldElem.attr("xlink:href") || "");`,
+    //         'window.registerImagePatternSvgImage(oldElem, newElem);',
+    //     );
+    // }
 
-    scNew.scriptFileItems.back2Array();
+    // scNew.scriptFileItems.back2Array();
 
-    window.modUtils.replaceFollowSC2DataInfo(scNew, scOld);
+    // window.modUtils.replaceFollowSC2DataInfo(scNew, scOld);
 
 })();
 
